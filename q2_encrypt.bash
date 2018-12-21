4f99104c439bb8326eb68feb2f2c2f1ff1629000
KC Tsiolis <kctsiolis@gmail.com>
2018-12-21 11:21:23 -0500

    Delete q2_encrypt.bash

--- q2_encrypt.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-small=`head -n1 $1` #Extracting small letters
-capital=`tail -n1 $1` #Extracting capital letters
-code=`echo $small$capital` #Concatenating all letters
-letters=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ #Ordered letters
-tr [$letters] [$code] <  $2  #Encrypting message file input
-
