7c360ae7047b6800d8469f16d35a2ef0554b7048
KC Tsiolis <kctsiolis@gmail.com>
2018-12-21 11:21:16 -0500

    Delete q2_decrypt.bash

--- q2_decrypt.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-small=`head -n1 $1` #Extracting small letters
-capital=`tail -n1 $1` #Extracting big letters
-code=`echo $small$capital` #Concatenating all letters
-letters=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ #Ordered letters
-tr [$code] [$letters] <  $2  #Decrypting message input
