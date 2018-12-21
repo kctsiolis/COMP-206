46f94680367209036171dfcdfb3f1065508acfa5
KC Tsiolis <kctsiolis@gmail.com>
2018-12-21 11:21:40 -0500

    Delete q3_image_sorter.bash

--- q3_image_sorter.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pictures=`find $1 -name "*.jpg"` #Searching for all pictures in directory and subdirectories
-sorted_pictures=`ls -Rtr $pictures` #Sorting the pictures by modification time from oldest to newest
-file_name_slashes=$1.jpg #Encoding the file name for the timeline
-file_name=`echo $file_name_slashes | tr '/' '_'` #Replacing slashes with underscores
-convert -append $sorted_pictures $file_name #Appending the sorted images into a timeline image
